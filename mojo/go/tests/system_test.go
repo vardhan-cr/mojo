ab2719d060a46bd34699aa840562c1d999fe308b
Jim Beveridge <jimbe@chromium.org>
2014-12-18 13:15:38 -0800

    Added golang support for additional params to MojoWait and MojoWaitMany.
    R=gautham.dorai@gmail.com, gauthamt@chromium.org, viettrungluu@chromium.org
    BUG=365436
    
    Review URL: https://codereview.chromium.org/800123003

--- mojo/go/tests/system_test.go
+++ mojo/go/tests/system_test.go
@@ -16,6 +16,14 @@ import (
 
 var core system.Core
 
+const (
+	MOJO_HANDLE_SIGNAL_READWRITABLE = m.MojoHandleSignals(m.MOJO_HANDLE_SIGNAL_READABLE |
+		m.MOJO_HANDLE_SIGNAL_WRITABLE)
+	MOJO_HANDLE_SIGNAL_ALL = m.MojoHandleSignals(m.MOJO_HANDLE_SIGNAL_READABLE |
+		m.MOJO_HANDLE_SIGNAL_WRITABLE |
+		m.MOJO_HANDLE_SIGNAL_PEER_CLOSED)
+)
+
 func init() {
 	embedder.InitializeMojoEmbedder()
 	core = m.GetCore()
@@ -31,6 +39,9 @@ func TestGetTimeTicksNow(t *testing.T) {
 func TestMessagePipe(t *testing.T) {
 	var h0, h1 m.MojoHandle
 	var r m.MojoResult
+	var index *uint32
+	var state *m.MojoHandleSignalsState
+	var states []m.MojoHandleSignalsState
 
 	if r, h0, h1 = core.CreateMessagePipe(nil); r != m.MOJO_RESULT_OK {
 		t.Fatalf("CreateMessagePipe failed:%v", r)
@@ -38,12 +49,35 @@ func TestMessagePipe(t *testing.T) {
 	if h0 == m.MOJO_HANDLE_INVALID || h1 == m.MOJO_HANDLE_INVALID {
 		t.Fatalf("CreateMessagePipe returned invalid handles h0:%v h1:%v", h0, h1)
 	}
-	if r = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_READABLE, 0); r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
+
+	r, state = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_READABLE, 0)
+	if r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
 		t.Fatalf("h0 should not be readable:%v", r)
 	}
-	if r = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_WRITABLE, 0); r != m.MOJO_RESULT_OK {
-		t.Fatalf("h0 should be writeable:%v", r)
+	if state == nil {
+		t.Fatalf("state should not be nil after CreateMessagePipe")
 	}
+	if state.SatisfiedSignals != m.MOJO_HANDLE_SIGNAL_WRITABLE {
+		t.Fatalf("state should be not be signaled readable after CreateMessagePipe:%v", state.SatisfiedSignals)
+	}
+	if state.SatisfiableSignals != MOJO_HANDLE_SIGNAL_ALL {
+		t.Fatalf("state should allow all signals after CreateMessagePipe:%v", state.SatisfiableSignals)
+	}
+
+	r, state = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_WRITABLE, 0)
+	if r != m.MOJO_RESULT_OK {
+		t.Fatalf("h0 should be writable:%v", r)
+	}
+	if state == nil {
+		t.Fatalf("state should not be nil after core.Wait")
+	}
+	if state.SatisfiedSignals != m.MOJO_HANDLE_SIGNAL_WRITABLE {
+		t.Fatalf("state should be signaled writable after core.Wait:%v", state.SatisfiedSignals)
+	}
+	if state.SatisfiableSignals != MOJO_HANDLE_SIGNAL_ALL {
+		t.Fatalf("state should allow all signals after core.Wait:%v", state.SatisfiableSignals)
+	}
+
 	if r, _, _, _, _ = core.ReadMessage(h0, m.MOJO_READ_MESSAGE_FLAG_NONE); r != m.MOJO_RESULT_SHOULD_WAIT {
 		t.Fatalf("Read on h0 did not return wait:%v", r)
 	}
@@ -51,9 +85,24 @@ func TestMessagePipe(t *testing.T) {
 	if r = core.WriteMessage(h1, kHello, nil, m.MOJO_WRITE_MESSAGE_FLAG_NONE); r != m.MOJO_RESULT_OK {
 		t.Fatalf("Failed WriteMessage on h1:%v", r)
 	}
-	if r = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_READABLE, m.MOJO_DEADLINE_INDEFINITE); r != m.MOJO_RESULT_OK {
+
+	r, state = core.Wait(h0, m.MOJO_HANDLE_SIGNAL_READABLE, m.MOJO_DEADLINE_INDEFINITE)
+	if r != m.MOJO_RESULT_OK {
 		t.Fatalf("h0 should be readable after WriteMessage to h1:%v", r)
 	}
+	if state == nil {
+		t.Fatalf("state should not be nil after WriteMessage to h1")
+	}
+	if state.SatisfiedSignals != MOJO_HANDLE_SIGNAL_READWRITABLE {
+		t.Fatalf("h0 should be signaled readable after WriteMessage to h1:%v", state.SatisfiedSignals)
+	}
+	if state.SatisfiableSignals != MOJO_HANDLE_SIGNAL_ALL {
+		t.Fatalf("h0 should be readable/writable after WriteMessage to h1:%v", state.SatisfiableSignals)
+	}
+	if !state.SatisfiableSignals.IsReadable() || !state.SatisfiableSignals.IsWritable() || !state.SatisfiableSignals.IsClosed() {
+		t.Fatalf("Helper functions are misbehaving")
+	}
+
 	r, msg, _, _, _ := core.ReadMessage(h0, m.MOJO_READ_MESSAGE_FLAG_NONE)
 	if r != m.MOJO_RESULT_OK {
 		t.Fatalf("Failed ReadMessage on h0:%v", r)
@@ -61,15 +110,42 @@ func TestMessagePipe(t *testing.T) {
 	if !bytes.Equal(msg, kHello) {
 		t.Fatalf("Invalid message expected:%s, got:%s", kHello, msg)
 	}
-	if r = core.WaitMany([]m.MojoHandle{h0}, []m.MojoHandleSignals{m.MOJO_HANDLE_SIGNAL_READABLE}, 10); r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
+
+	r, index, states = core.WaitMany([]m.MojoHandle{h0}, []m.MojoHandleSignals{m.MOJO_HANDLE_SIGNAL_READABLE}, 10)
+	if r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
 		t.Fatalf("h0 should not be readable after reading message:%v", r)
 	}
+	if index != nil {
+		t.Fatalf("should be no index after MOJO_RESULT_DEADLINE_EXCEEDED:%v", index)
+	}
+	if states == nil || len(states) != 1 {
+		t.Fatalf("states should be set after WaitMany:%v", states)
+	}
+	if states[0].SatisfiedSignals != m.MOJO_HANDLE_SIGNAL_WRITABLE {
+		t.Fatalf("h0 should be signaled readable WaitMany:%v", states[0].SatisfiedSignals)
+	}
+	if states[0].SatisfiableSignals != MOJO_HANDLE_SIGNAL_ALL {
+		t.Fatalf("h0 should be readable/writable after WaitMany:%v", states[0].SatisfiableSignals)
+	}
+
 	if r = core.Close(h0); r != m.MOJO_RESULT_OK {
 		t.Fatalf("Close on h0 failed:%v", r)
 	}
-	if r = core.Wait(h1, m.MOJO_HANDLE_SIGNAL_READABLE|m.MOJO_HANDLE_SIGNAL_WRITABLE, 100); r != m.MOJO_RESULT_FAILED_PRECONDITION {
-		t.Fatalf("h1 should not be readable/writeable after h0 has been closed:%v", r)
+
+	r, state = core.Wait(h1, MOJO_HANDLE_SIGNAL_READWRITABLE, 100)
+	if r != m.MOJO_RESULT_FAILED_PRECONDITION {
+		t.Fatalf("h1 should not be readable/writable after Close(h0):%v", r)
 	}
+	if state == nil {
+		t.Fatalf("state should not be nil after Close(h0)")
+	}
+	if state.SatisfiedSignals != m.MOJO_HANDLE_SIGNAL_PEER_CLOSED {
+		t.Fatalf("state should be signaled closed after Close(h0):%v", state.SatisfiedSignals)
+	}
+	if state.SatisfiableSignals != m.MOJO_HANDLE_SIGNAL_PEER_CLOSED {
+		t.Fatalf("state should only be closable after Close(h0):%v", state.SatisfiableSignals)
+	}
+
 	if r = core.Close(h1); r != m.MOJO_RESULT_OK {
 		t.Fatalf("Close on h1 failed:%v", r)
 	}
@@ -85,10 +161,10 @@ func TestDataPipe(t *testing.T) {
 	if hp == m.MOJO_HANDLE_INVALID || hc == m.MOJO_HANDLE_INVALID {
 		t.Fatalf("CreateDataPipe returned invalid handles hp:%v hc:%v", hp, hc)
 	}
-	if r = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 0); r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
+	if r, _ = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 0); r != m.MOJO_RESULT_DEADLINE_EXCEEDED {
 		t.Fatalf("hc should not be readable:%v", r)
 	}
-	if r = core.Wait(hp, m.MOJO_HANDLE_SIGNAL_WRITABLE, 0); r != m.MOJO_RESULT_OK {
+	if r, _ = core.Wait(hp, m.MOJO_HANDLE_SIGNAL_WRITABLE, 0); r != m.MOJO_RESULT_OK {
 		t.Fatalf("hp should be writeable:%v", r)
 	}
 	kHello := []byte("hello")
@@ -96,7 +172,7 @@ func TestDataPipe(t *testing.T) {
 	if r != m.MOJO_RESULT_OK || numBytes != (uint32)(len(kHello)) {
 		t.Fatalf("Failed WriteData on hp:%v numBytes:%d", r, numBytes)
 	}
-	if r = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 1000); r != m.MOJO_RESULT_OK {
+	if r, _ = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 1000); r != m.MOJO_RESULT_OK {
 		t.Fatalf("hc should be readable after WriteData on hp:%v", r)
 	}
 	r, data := core.ReadData(hc, m.MOJO_READ_DATA_FLAG_NONE)
@@ -109,7 +185,7 @@ func TestDataPipe(t *testing.T) {
 	if r = core.Close(hp); r != m.MOJO_RESULT_OK {
 		t.Fatalf("Close on hp failed:%v", r)
 	}
-	if r = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 100); r != m.MOJO_RESULT_FAILED_PRECONDITION {
+	if r, _ = core.Wait(hc, m.MOJO_HANDLE_SIGNAL_READABLE, 100); r != m.MOJO_RESULT_FAILED_PRECONDITION {
 		t.Fatalf("hc should not be readable after hp closed:%v", r)
 	}
 	if r = core.Close(hc); r != m.MOJO_RESULT_OK {
