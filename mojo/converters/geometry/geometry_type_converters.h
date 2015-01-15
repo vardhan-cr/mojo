a229acb4197a27e6be312ab7171488da9c50632e
Adam Barth <abarth@chromium.org>
2015-01-15 11:09:56 -0800

    The Sky compositor should recycle textures
    Previously, we were creating a new texture for every frame. Using tracing on a
    Nexus 5, that appears to cost about 6ms per frame on the GPU thread. After this
    CL, we keep a cache of recently used textures and only allocate a new one when
    either (1) we don't have one free or (2) our size requirements have changed.
    
    If our size requirements change, we dump the whole texture cache. In the
    future, we'll probably need something fancier if we ever need more than one
    size texture per frame.
    
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/845963006

--- mojo/converters/geometry/geometry_type_converters.h
+++ mojo/converters/geometry/geometry_type_converters.h
@@ -89,4 +89,12 @@ struct MOJO_GEOMETRY_EXPORT TypeConverter<gfx::Rect, Rect> {
 
 }  // namespace mojo
 
+inline bool operator==(const gfx::Size& lhs, const mojo::Size& rhs) {
+  return lhs.width() == rhs.width && lhs.height() == rhs.height;
+}
+
+inline bool operator==(const mojo::Size& lhs, const gfx::Size& rhs) {
+  return rhs == lhs;
+}
+
 #endif  // MOJO_CONVERTERS_GEOMETRY_GEOMETRY_TYPE_CONVERTERS_H_
