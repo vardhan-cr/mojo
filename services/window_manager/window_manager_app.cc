vardhan-cr/mojo

commit 93d013c2dc798e50cd60f3c85bf859fc98a264f8

Add window_manager application tests.
Add a simple WindowManager application test for Embed.
The test just embeds its own ApplicationDelegate's view.

Remove WindowManagerApp View observation on destruction.
(it may be destroyed before the Views that it observes)
Only quit the message loop as needed on VM disconnect.
(the message loop may be quit and destroyed earlier)
Also unregister the root view in OnViewDestroying.
(to remove observation and the entry in the id map)

Add example View placement to DefaultWindowManager.
(this was just for visual inspection, not critical)
Handle null focus/capture controllers in ViewTargeter.
(to avoid crashes on visual inspection, not critical)

Other miscellaneous cleanup.

BUG=392646
TEST=mojo/tools/mojob.py test runs this apptest.
R=sky@chromium.org

Review URL: https://codereview.chromium.org/699443004

services/window_manager/window_manager_app.cc
@@ -56,10 +56,24 @@ WindowManagerApp::WindowManagerApp(
       native_viewport_event_dispatcher_factory_(this),
       wrapped_view_manager_delegate_(view_manager_delegate),
       window_manager_delegate_(window_manager_delegate),
-      root_(nullptr) {
+      root_(nullptr),
+      gesture_provider_(this) {
 }
 
 WindowManagerApp::~WindowManagerApp() {
+  // TODO(msw|sky): Should this destructor explicitly delete the ViewManager?
+  mojo::ViewManager* cached_view_manager = view_manager();
+  for (RegisteredViewIdSet::const_iterator it = registered_view_id_set_.begin();
+       cached_view_manager && it != registered_view_id_set_.end(); ++it) {
+    View* view = cached_view_manager->GetViewById(*it);
+    if (view && view == root_)
+      root_ = nullptr;
+    if (view)
+      view->RemoveObserver(this);
+  }
+  registered_view_id_set_.clear();
+  DCHECK(!root_);
+
   STLDeleteElements(&connections_);
   ui::GestureRecognizer::Get()->CleanupStateForConsumer(this);
 }
@@ -160,13 +174,12 @@ void WindowManagerApp::OnEmbed(View* root,
 
 void WindowManagerApp::OnViewManagerDisconnected(
     mojo::ViewManager* view_manager) {
-  DCHECK(root_);
-  DCHECK(this->view_manager());
-  DCHECK_EQ(this->view_manager(), view_manager);
   if (wrapped_view_manager_delegate_)
     wrapped_view_manager_delegate_->OnViewManagerDisconnected(view_manager);
-  root_ = nullptr;
-  base::MessageLoop::current()->Quit();
+
+  base::MessageLoop* message_loop = base::MessageLoop::current();
+  if (message_loop && message_loop->is_running())
+    message_loop->Quit();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -194,15 +207,14 @@ void WindowManagerApp::OnTreeChanged(
 }
 
 void WindowManagerApp::OnViewDestroying(View* view) {
-  if (view != root_) {
-    Unregister(view);
-    return;
+  Unregister(view);
+  if (view == root_) {
+    root_ = nullptr;
+    if (focus_controller_)
+      focus_controller_->RemoveObserver(this);
+    if (capture_controller_)
+      capture_controller_->RemoveObserver(this);
   }
-  root_ = nullptr;
-  if (focus_controller_)
-    focus_controller_->RemoveObserver(this);
-  if (capture_controller_)
-    capture_controller_->RemoveObserver(this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

examples/wm_flow/app/app.cc
@@ -63,7 +63,7 @@ class WMFlowApp : public mojo::ApplicationDelegate,
  private:
   typedef std::map<mojo::View*, mojo::BitmapUploader*> ViewToUploader;
 
-  // Overridden from Application:
+  // Overridden from ApplicationDelegate:
   virtual void Initialize(mojo::ApplicationImpl* app) override {
     shell_ = app->shell();
     view_manager_client_factory_.reset(
@@ -81,8 +81,6 @@ class WMFlowApp : public mojo::ApplicationDelegate,
     connection->AddService(view_manager_client_factory_.get());
     return true;
   }
-
-  void OnConnect(bool success) {}
 
   // Overridden from mojo::ViewManagerDelegate:
   virtual void OnEmbed(

mojo/BUILD.gn
@@ -68,6 +68,7 @@ group("tests") {
     "//services/js:js_apptests",
     "//services/view_manager:view_manager_service_apptests",
     "//services/view_manager:view_manager_service_unittests",
+    "//services/window_manager:window_manager_apptests",
     "//services/window_manager:window_manager_unittests",
   ]
 

mojo/tools/data/apptests
@@ -29,4 +29,7 @@
   {
     "test": "mojo:js_apptests",
   },
+  {
+    "test": "mojo:window_manager_apptests",
+  },
 ]

services/view_manager/view_manager_app.cc
@@ -4,7 +4,6 @@
 
 #include "services/view_manager/view_manager_app.h"
 
-#include "mojo/application/application_runner_chromium.h"
 #include "mojo/common/tracing_impl.h"
 #include "mojo/public/c/system/main.h"
 #include "mojo/public/cpp/application/application_connection.h"
@@ -22,8 +21,7 @@ using mojo::WindowManagerInternalClient;
 
 namespace view_manager {
 
-ViewManagerApp::ViewManagerApp() : wm_app_connection_(nullptr) {
-}
+ViewManagerApp::ViewManagerApp() : wm_app_connection_(nullptr) {}
 ViewManagerApp::~ViewManagerApp() {}
 
 void ViewManagerApp::Initialize(ApplicationImpl* app) {

services/window_manager/BUILD.gn
@@ -17,7 +17,6 @@ mojo_native_application("window_manager") {
   deps = [
     "//base",
     "//mojo/application",
-    "//mojo/common:tracing_impl",
     "//mojo/services/view_manager/public/cpp",
   ]
 }
@@ -102,4 +101,27 @@ test("window_manager_unittests") {
   if (use_x11) {
     deps += [ "//ui/gfx/x" ]
   }
+}
+
+mojo_native_application("window_manager_apptests") {
+  testonly = true
+
+  sources = [
+    "window_manager_apptest.cc",
+  ]
+
+  deps = [
+    "//base",
+    "//mojo/application",
+    "//mojo/application:test_support",
+    "//mojo/environment:chromium",
+    "//mojo/public/c/system:for_shared_library",
+    "//mojo/public/cpp/system:system",
+    "//mojo/services/view_manager/public/cpp",
+    "//mojo/services/window_manager/public/interfaces",
+  ]
+
+  datadeps = [
+    ":window_manager",
+  ]
 }

services/window_manager/main.cc
@@ -4,7 +4,6 @@
 
 #include "base/memory/scoped_ptr.h"
 #include "mojo/application/application_runner_chromium.h"
-#include "mojo/common/tracing_impl.h"
 #include "mojo/public/c/system/main.h"
 #include "mojo/public/cpp/application/application_delegate.h"
 #include "mojo/public/cpp/application/service_provider_impl.h"
@@ -27,14 +26,16 @@ class DefaultWindowManager : public mojo::ApplicationDelegate,
                              public WindowManagerDelegate {
  public:
   DefaultWindowManager()
-      : window_manager_app_(new WindowManagerApp(this, this)), root_(nullptr) {}
+      : window_manager_app_(new WindowManagerApp(this, this)),
+        root_(nullptr),
+        window_offset_(10) {
+  }
   ~DefaultWindowManager() override {}
 
  private:
   // Overridden from mojo::ApplicationDelegate:
   void Initialize(mojo::ApplicationImpl* impl) override {
     window_manager_app_->Initialize(impl);
-    mojo::TracingImpl::Create(impl);
   }
   bool ConfigureIncomingConnection(
       mojo::ApplicationConnection* connection) override {
@@ -54,8 +55,16 @@ class DefaultWindowManager : public mojo::ApplicationDelegate,
   void Embed(
       const mojo::String& url,
       mojo::InterfaceRequest<mojo::ServiceProvider> service_provider) override {
+    DCHECK(root_);
     View* view = root_->view_manager()->CreateView();
     root_->AddChild(view);
+
+    mojo::Rect rect;
+    rect.x = rect.y = window_offset_;
+    rect.width = rect.height = 100;
+    view->SetBounds(rect);
+    window_offset_ += 10;
+
     view->SetVisible(true);
     view->Embed(url, scoped_ptr<mojo::ServiceProviderImpl>(
         new mojo::ServiceProviderImpl).Pass());
@@ -64,6 +73,7 @@ class DefaultWindowManager : public mojo::ApplicationDelegate,
   scoped_ptr<WindowManagerApp> window_manager_app_;
 
   View* root_;
+  int window_offset_;
 
   MOJO_DISALLOW_COPY_AND_ASSIGN(DefaultWindowManager);
 };

services/window_manager/view_targeter.cc
@@ -73,10 +73,11 @@ bool ViewTargeter::EventLocationInsideBounds(
 ViewTarget* ViewTargeter::FindTargetForKeyEvent(ViewTarget* view_target,
                                                 const ui::KeyEvent& key) {
   FocusController* focus_controller = GetFocusController(view_target->view());
-  mojo::View* focused_view = focus_controller->GetFocusedView();
-  if (focused_view)
-    return ViewTarget::TargetFromView(focused_view);
-
+  if (focus_controller) {
+    mojo::View* focused_view = focus_controller->GetFocusedView();
+    if (focused_view)
+      return ViewTarget::TargetFromView(focused_view);
+  }
   return view_target;
 }
 
@@ -88,10 +89,11 @@ ViewTarget* ViewTargeter::FindTargetInRootView(ViewTarget* root_view,
 
   CaptureController* capture_controller =
       GetCaptureController(root_view->view());
-  DCHECK(capture_controller);
-  mojo::View* capture_view = capture_controller->GetCapture();
-  if (capture_view)
-    return ViewTarget::TargetFromView(capture_view);
+  if (capture_controller) {
+    mojo::View* capture_view = capture_controller->GetCapture();
+    if (capture_view)
+      return ViewTarget::TargetFromView(capture_view);
+  }
 
   // TODO(erg): There's a whole bunch of junk about handling touch events
   // here. Handle later.

commit bae17947de96ac0e545cce36dce6e6e52b11caf2

Fix build break from a bad rebase.
Broken in 93d013c2dc798e50cd60f3c85bf859fc98a264f8
Bad rebase of services/window_manager/window_manager_app.cc

BUG=NONE
TBR=sky@chromium.org

Review URL: https://codereview.chromium.org/831663006

services/window_manager/window_manager_app.cc
@@ -56,8 +56,7 @@ WindowManagerApp::WindowManagerApp(
       native_viewport_event_dispatcher_factory_(this),
       wrapped_view_manager_delegate_(view_manager_delegate),
       window_manager_delegate_(window_manager_delegate),
-      root_(nullptr),
-      gesture_provider_(this) {
+      root_(nullptr) {
 }
 
 WindowManagerApp::~WindowManagerApp() {