5c302423e201e40ba4995c8fac1d7458db1969df
Hans Muller <hansmuller@chromium.org>
2015-01-27 11:27:19 -0800

    This is a content handler that forwards to another "target" application. The content handler reads the target application's URL from the response and then connects to it from AcceptConnection():
    void AcceptConnection(
        ApplicationImpl* app,
        const String& requestor_url,
        InterfaceRequest<mojo::ServiceProvider> services,
        ServiceProviderPtr exposed_services) {
      app->shell()->ConnectToApplication(target_url_, services.Pass(), exposed_services.Pass());
    }
    
    I made ApplicationImpl::AcceptConnection() protected (it was private) to enable this.
    
    This does work. If I embed a URL for something like the following
    in a View:
    
      #!mojo mojo:forwarding_content_handler
      https://target_url.mojo
    
    The Mojo app at target_url.mojo ends up in the View.
    
    BUG=
    R=aa@chromium.org
    
    Review URL: https://codereview.chromium.org/877553005

---
+++ examples/forwarding_content_handler/forwarding_content_handler.cc
@@ -0,0 +1,82 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
+#include "mojo/application/application_runner_chromium.h"
+#include "mojo/application/content_handler_factory.h"
+#include "mojo/common/data_pipe_utils.h"
+#include "mojo/public/c/system/main.h"
+#include "mojo/public/cpp/application/application_connection.h"
+#include "mojo/public/cpp/application/application_delegate.h"
+#include "mojo/public/cpp/application/application_impl.h"
+#include "mojo/public/cpp/application/interface_factory_impl.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/services/content_handler/public/interfaces/content_handler.mojom.h"
+
+namespace mojo {
+namespace examples {
+
+class ForwardingApplicationImpl : public ApplicationImpl,
+                                  public ApplicationDelegate {
+ public:
+  ForwardingApplicationImpl(InterfaceRequest<Application> request,
+                            std::string target_url)
+      : ApplicationImpl(this, request.Pass()),
+        target_url_(target_url) {
+  }
+
+ private:
+  // ApplicationImpl:
+  void AcceptConnection(const String& requestor_url,
+                        InterfaceRequest<ServiceProvider> services,
+                        ServiceProviderPtr exposed_services) override {
+    shell()->ConnectToApplication(target_url_, services.Pass(),
+                                  exposed_services.Pass());
+  }
+
+  std::string target_url_;
+};
+
+class ForwardingContentHandler : public ApplicationDelegate,
+                                 public ContentHandlerFactory::ManagedDelegate {
+ public:
+  ForwardingContentHandler() : content_handler_factory_(this) {}
+
+ private:
+  // Overridden from ApplicationDelegate:
+  virtual bool ConfigureIncomingConnection(
+      ApplicationConnection* connection) override {
+    connection->AddService(&content_handler_factory_);
+    return true;
+  }
+
+  // Overridden from ContentHandlerFactory::ManagedDelegate:
+  virtual scoped_ptr<ContentHandlerFactory::HandledApplicationHolder>
+  CreateApplication(InterfaceRequest<Application> application_request,
+                    URLResponsePtr response) override {
+    CHECK(!response.is_null());
+    const std::string requestor_url(response->url);
+    std::string target_url;
+    if(!common::BlockingCopyToString(response->body.Pass(), &target_url)) {
+      LOG(WARNING) << "unable to read target URL from " << requestor_url;
+      return nullptr;
+    }
+    return make_handled_factory_holder(
+        new ForwardingApplicationImpl(application_request.Pass(), target_url));
+  }
+
+  ContentHandlerFactory content_handler_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(ForwardingContentHandler);
+};
+
+}  // namespace examples
+}  // namespace mojo
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  mojo::ApplicationRunnerChromium runner(
+      new mojo::examples::ForwardingContentHandler());
+  return runner.Run(shell_handle);
+}
