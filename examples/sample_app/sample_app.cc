vardhan-cr/mojo

commit 91b812b1af824c8faba2f75d88d31a1532164728

Remove NativeViewportClient
This removes the client interface from NativeViewport. This interface
had two methods, OnDestroyed and OnMetricsChanged. OnDestroyed is
redundant with just closing the message pipe. OnMetricsChanged is more
interesting. Since there's no rate limiting in this interface, an
unbounded number of metrics updates could accumulate in this pipe. Since
users of the NativeViewport service will (generally) only care about
the most recent update this replaces the pipe with a request->callback
pair NativeViewport.RequestMetrics. The reply message to this is sent
whenever the metrics change. Callers have been updated to either use
the metrics from the initial creation callback (for samples that do not
handle resize) or continually call RequestMetrics if they wish to track
size / scale changes. This way only one request is pending per
connection.

BUG=451319
R=sky@chromium.org

Review URL: https://codereview.chromium.org/878933005

examples/sample_app/sample_app.cc
@@ -6,7 +6,7 @@
 #include <string>
 
 #include "base/bind.h"
-#include "base/memory/weak_ptr.h"
+#include "base/memory/scoped_ptr.h"
 #include "examples/sample_app/gles2_client_impl.h"
 #include "mojo/public/c/system/main.h"
 #include "mojo/public/cpp/application/application_connection.h"
@@ -21,12 +21,11 @@
 
 namespace examples {
 
-class SampleApp
-    : public mojo::ApplicationDelegate,
-      public mojo::NativeViewportClient,
-      public mojo::InterfaceImpl<mojo::NativeViewportEventDispatcher> {
+class SampleApp : public mojo::ApplicationDelegate,
+                  public mojo::NativeViewportEventDispatcher,
+                  public mojo::ErrorHandler {
  public:
-  SampleApp() : weak_factory_(this) {}
+  SampleApp() : dispatcher_binding_(this) {}
 
   ~SampleApp() override {
     // TODO(darin): Fix shutdown so we don't need to leak this.
@@ -35,7 +34,7 @@ class SampleApp
 
   void Initialize(mojo::ApplicationImpl* app) override {
     app->ConnectToService("mojo:native_viewport_service", &viewport_);
-    viewport_.set_client(this);
+    viewport_.set_error_handler(this);
 
     SetEventDispatcher();
 
@@ -47,16 +46,16 @@ class SampleApp
     size->height = 600;
     viewport_->Create(size.Pass(),
                       base::Bind(&SampleApp::OnCreatedNativeViewport,
-                                 weak_factory_.GetWeakPtr()));
+                                 base::Unretained(this)));
     viewport_->Show();
   }
 
-  void OnDestroyed() override { mojo::RunLoop::current()->Quit(); }
-
-  void OnMetricsChanged(mojo::ViewportMetricsPtr metrics) override {
+  void OnMetricsChanged(mojo::ViewportMetricsPtr metrics) {
     assert(metrics);
     if (gles2_client_)
       gles2_client_->SetSize(*metrics->size);
+    viewport_->RequestMetrics(
+        base::Bind(&SampleApp::OnMetricsChanged, base::Unretained(this)));
   }
 
   void OnEvent(mojo::EventPtr event,
@@ -69,28 +68,31 @@ class SampleApp
 
  private:
   void SetEventDispatcher() {
-    mojo::NativeViewportEventDispatcherPtr proxy;
-    mojo::WeakBindToProxy(this, &proxy);
-    viewport_->SetEventDispatcher(proxy.Pass());
+    mojo::NativeViewportEventDispatcherPtr ptr;
+    dispatcher_binding_.Bind(GetProxy(&ptr));
+    viewport_->SetEventDispatcher(ptr.Pass());
   }
 
-  void OnCreatedNativeViewport(uint64_t native_viewport_id) {
-    mojo::SizePtr size = mojo::Size::New();
-    size->width = 800;
-    size->height = 600;
+  void OnCreatedNativeViewport(uint64_t native_viewport_id,
+                               mojo::ViewportMetricsPtr metrics) {
     mojo::ViewportParameterListenerPtr listener;
     mojo::CommandBufferPtr command_buffer;
     // TODO(jamesr): Output to a surface instead.
-    gpu_service_->CreateOnscreenGLES2Context(native_viewport_id, size.Pass(),
-                                             GetProxy(&command_buffer),
-                                             listener.Pass());
+    gpu_service_->CreateOnscreenGLES2Context(
+        native_viewport_id, metrics->size.Clone(), GetProxy(&command_buffer),
+        listener.Pass());
     gles2_client_.reset(new GLES2ClientImpl(command_buffer.Pass()));
+    viewport_->RequestMetrics(
+        base::Bind(&SampleApp::OnMetricsChanged, base::Unretained(this)));
   }
+
+  // ErrorHandler implementation.
+  void OnConnectionError() override { mojo::RunLoop::current()->Quit(); }
 
   scoped_ptr<GLES2ClientImpl> gles2_client_;
   mojo::NativeViewportPtr viewport_;
   mojo::GpuPtr gpu_service_;
-  base::WeakPtrFactory<SampleApp> weak_factory_;
+  mojo::Binding<NativeViewportEventDispatcher> dispatcher_binding_;
 
   DISALLOW_COPY_AND_ASSIGN(SampleApp);
 };

examples/surfaces_app/surfaces_app.cc
@@ -27,9 +27,7 @@ namespace examples {
 
 static const uint32_t kLocalId = 1u;
 
-class SurfacesApp : public ApplicationDelegate,
-                    public SurfaceClient,
-                    public NativeViewportClient {
+class SurfacesApp : public ApplicationDelegate, public SurfaceClient {
  public:
   SurfacesApp() : id_namespace_(0u), weak_factory_(this) {}
   ~SurfacesApp() override {}
@@ -37,7 +35,6 @@ class SurfacesApp : public ApplicationDelegate,
   // ApplicationDelegate implementation
   bool ConfigureIncomingConnection(ApplicationConnection* connection) override {
     connection->ConnectToService("mojo:native_viewport_service", &viewport_);
-    viewport_.set_client(this);
 
     connection->ConnectToService("mojo:surfaces_service", &surface_);
     surface_.set_client(this);
@@ -87,6 +84,7 @@ class SurfacesApp : public ApplicationDelegate,
         base::TimeDelta::FromMilliseconds(50));
   }
 
+ private:
   // SurfaceClient implementation.
   void SetIdNamespace(uint32_t id_namespace) override {
     auto qualified_id = mojo::SurfaceId::New();
@@ -97,12 +95,8 @@ class SurfacesApp : public ApplicationDelegate,
   void ReturnResources(Array<ReturnedResourcePtr> resources) override {
     DCHECK(!resources.size());
   }
-  // NativeViewportClient implementation.
-  void OnMetricsChanged(mojo::ViewportMetricsPtr metrics) override {}
-  void OnDestroyed() override {}
-
- private:
-  void OnCreatedNativeViewport(uint64_t native_viewport_id) {}
+  void OnCreatedNativeViewport(uint64_t native_viewport_id,
+                               mojo::ViewportMetricsPtr metrics) {}
 
   SurfacePtr surface_;
   uint32_t id_namespace_;

mojo/services/native_viewport/public/interfaces/native_viewport.mojom
@@ -14,26 +14,26 @@ struct ViewportMetrics {
   float device_pixel_ratio = 1.0;
 };
 
-[Client=NativeViewportClient]
 interface NativeViewport {
   // TODO(sky): having a create function is awkward. Should there be a factory
   // to create the NativeViewport that takes the size?
-  Create(Size size) => (uint64 native_viewport_id);
+  Create(Size size) => (uint64 native_viewport_id, ViewportMetrics metrics);
+
   Show();
   Hide();
   Close();
   SetSize(Size size);
   SubmittedFrame(SurfaceId surface_id);
   SetEventDispatcher(NativeViewportEventDispatcher dispatcher);
+
+  // The initial viewport metrics will be sent in the reply to the Create
+  // method. Call RequestMetrics() to receive updates when the viewport metrics
+  // change. The reply will be sent when the viewport metrics are different from
+  // the values last sent, so to receive continuous updates call this method
+  // again after receiving the callback.
+  RequestMetrics() => (ViewportMetrics metrics);
 };
 
 interface NativeViewportEventDispatcher {
   OnEvent(Event event) => ();
-};
-
-interface NativeViewportClient {
-  // OnMetricsAvailable() is sent at least once after the callback from Create()
-  // is called.
-  OnMetricsChanged(ViewportMetrics metrics);
-  OnDestroyed();
 };

services/native_viewport/main.cc
@@ -55,7 +55,7 @@ class NativeViewportAppDelegate : public mojo::ApplicationDelegate,
   // mojo::InterfaceFactory<NativeViewport> implementation.
   void Create(ApplicationConnection* connection,
               mojo::InterfaceRequest<NativeViewport> request) override {
-    BindToRequest(new NativeViewportImpl(app_, is_headless_), &request);
+    new NativeViewportImpl(app_, is_headless_, request.Pass());
   }
 
   // mojo::InterfaceFactory<Gpu> implementation.

services/native_viewport/native_viewport_impl.cc
@@ -30,13 +30,18 @@ bool IsRateLimitedEventType(ui::Event* event) {
 
 }  // namespace
 
-NativeViewportImpl::NativeViewportImpl(mojo::ApplicationImpl* app,
-                                       bool is_headless)
+NativeViewportImpl::NativeViewportImpl(
+    mojo::ApplicationImpl* app,
+    bool is_headless,
+    mojo::InterfaceRequest<mojo::NativeViewport> request)
     : is_headless_(is_headless),
       widget_id_(0u),
+      sent_metrics_(false),
       metrics_(mojo::ViewportMetrics::New()),
       waiting_for_event_ack_(false),
+      binding_(this, request.Pass()),
       weak_factory_(this) {
+  binding_.set_error_handler(this);
   app->ConnectToService("mojo:surfaces_service", &surface_);
   // TODO(jamesr): Should be mojo_gpu_service
   app->ConnectToService("mojo:native_viewport_service", &gpu_service_);
@@ -50,7 +55,8 @@ NativeViewportImpl::~NativeViewportImpl() {
 
 void NativeViewportImpl::Create(
     mojo::SizePtr size,
-    const mojo::Callback<void(uint64_t)>& callback) {
+    const mojo::Callback<void(uint64_t, mojo::ViewportMetricsPtr metrics)>&
+        callback) {
   create_callback_ = callback;
   metrics_->size = size.Clone();
   if (is_headless_)
@@ -58,6 +64,15 @@ void NativeViewportImpl::Create(
   else
     platform_viewport_ = PlatformViewport::Create(this);
   platform_viewport_->Init(gfx::Rect(size.To<gfx::Size>()));
+}
+
+void NativeViewportImpl::RequestMetrics(const MetricsCallback& callback) {
+  if (!sent_metrics_) {
+    callback.Run(metrics_.Clone());
+    sent_metrics_ = true;
+    return;
+  }
+  metrics_callback_ = callback;
 }
 
 void NativeViewportImpl::Show() {
@@ -104,21 +119,24 @@ void NativeViewportImpl::OnMetricsChanged(mojo::ViewportMetricsPtr metrics) {
     return;
 
   metrics_ = metrics.Pass();
+  sent_metrics_ = false;
 
-  // Wait for the accelerated widget before telling the client of the bounds.
-  if (create_callback_.is_null())
-    ProcessOnMetricsChanged();
+  if (!metrics_callback_.is_null()) {
+    metrics_callback_.Run(metrics_.Clone());
+    metrics_callback_.reset();
+    sent_metrics_ = true;
+  }
+  if (viewport_surface_)
+    viewport_surface_->SetSize(metrics_->size.To<gfx::Size>());
 }
 
 void NativeViewportImpl::OnAcceleratedWidgetAvailable(
     gfx::AcceleratedWidget widget) {
   widget_id_ = static_cast<uint64_t>(bit_cast<uintptr_t>(widget));
   // TODO(jamesr): Remove once everything is converted to surfaces.
-  create_callback_.Run(widget_id_);
+  create_callback_.Run(widget_id_, metrics_.Clone());
+  sent_metrics_ = true;
   create_callback_.reset();
-  // Immediately tell the client the metrics. The size may be wrong, if so we'll
-  // get the right one in the next OnMetricsChanged() call.
-  ProcessOnMetricsChanged();
   if (viewport_surface_)
     viewport_surface_->SetWidgetId(widget_id_);
 }
@@ -152,17 +170,17 @@ bool NativeViewportImpl::OnEvent(ui::Event* ui_event) {
 }
 
 void NativeViewportImpl::OnDestroyed() {
-  client()->OnDestroyed();
+  // This will signal a connection error and cause us to delete |this|.
+  binding_.Close();
+}
+
+void NativeViewportImpl::OnConnectionError() {
+  binding_.set_error_handler(nullptr);
+  delete this;
 }
 
 void NativeViewportImpl::AckEvent() {
   waiting_for_event_ack_ = false;
-}
-
-void NativeViewportImpl::ProcessOnMetricsChanged() {
-  client()->OnMetricsChanged(metrics_.Clone());
-  if (viewport_surface_)
-    viewport_surface_->SetSize(metrics_->size.To<gfx::Size>());
 }
 
 }  // namespace native_viewport

services/native_viewport/native_viewport_impl.h
@@ -8,6 +8,7 @@
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "cc/surfaces/surface_id.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
 #include "mojo/services/gpu/public/interfaces/gpu.mojom.h"
 #include "mojo/services/native_viewport/public/interfaces/native_viewport.mojom.h"
 #include "mojo/services/surfaces/public/interfaces/surfaces.mojom.h"
@@ -25,15 +26,27 @@ class ApplicationImpl;
 namespace native_viewport {
 class ViewportSurface;
 
-class NativeViewportImpl : public mojo::InterfaceImpl<mojo::NativeViewport>,
-                           public PlatformViewport::Delegate {
+// A NativeViewportImpl is bound to a message pipe and to a PlatformViewport.
+// The NativeViewportImpl's lifetime ends when either the message pipe is closed
+// or the PlatformViewport informs the NativeViewportImpl that it has been
+// destroyed.
+class NativeViewportImpl : public mojo::NativeViewport,
+                           public PlatformViewport::Delegate,
+                           public mojo::ErrorHandler {
+  using CreateCallback = mojo::Callback<void(uint64_t native_viewport_id,
+                                             mojo::ViewportMetricsPtr metrics)>;
+  using MetricsCallback =
+      mojo::Callback<void(mojo::ViewportMetricsPtr metrics)>;
+
  public:
-  NativeViewportImpl(mojo::ApplicationImpl* app, bool is_headless);
+  NativeViewportImpl(mojo::ApplicationImpl* app,
+                     bool is_headless,
+                     mojo::InterfaceRequest<mojo::NativeViewport> request);
   ~NativeViewportImpl() override;
 
   // InterfaceImpl<NativeViewport> implementation.
-  void Create(mojo::SizePtr size,
-              const mojo::Callback<void(uint64_t)>& callback) override;
+  void Create(mojo::SizePtr size, const CreateCallback& callback) override;
+  void RequestMetrics(const MetricsCallback& callback) override;
   void Show() override;
   void Hide() override;
   void Close() override;
@@ -48,22 +61,26 @@ class NativeViewportImpl : public mojo::InterfaceImpl<mojo::NativeViewport>,
   bool OnEvent(ui::Event* ui_event) override;
   void OnDestroyed() override;
 
+  // mojo::ErrorHandler implementation.
+  void OnConnectionError() override;
+
   void AckEvent();
 
  private:
-  void ProcessOnMetricsChanged();
-
   bool is_headless_;
   scoped_ptr<PlatformViewport> platform_viewport_;
   scoped_ptr<ViewportSurface> viewport_surface_;
   uint64_t widget_id_;
+  bool sent_metrics_;
   mojo::ViewportMetricsPtr metrics_;
   mojo::GpuPtr gpu_service_;
   mojo::SurfacePtr surface_;
   cc::SurfaceId child_surface_id_;
   bool waiting_for_event_ack_;
-  mojo::Callback<void(uint64_t)> create_callback_;
+  CreateCallback create_callback_;
+  MetricsCallback metrics_callback_;
   mojo::NativeViewportEventDispatcherPtr event_dispatcher_;
+  mojo::Binding<mojo::NativeViewport> binding_;
   base::WeakPtrFactory<NativeViewportImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeViewportImpl);

services/native_viewport/platform_viewport_win.cc
@@ -1,107 +1,0 @@
-// Copyright 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "services/native_viewport/platform_viewport.h"
-
-#include "base/memory/scoped_ptr.h"
-#include "mojo/converters/geometry/geometry_type_converters.h"
-#include "ui/gfx/rect.h"
-#include "ui/platform_window/platform_window_delegate.h"
-#include "ui/platform_window/win/win_window.h"
-
-namespace mojo {
-
-class PlatformViewportWin : public PlatformViewport,
-                            public ui::PlatformWindowDelegate {
- public:
-  explicit PlatformViewportWin(Delegate* delegate)
-      : delegate_(delegate) {
-  }
-
-  virtual ~PlatformViewportWin() {
-    // Destroy the platform-window while |this| is still alive.
-    platform_window_.reset();
-  }
-
- private:
-  // Overridden from PlatformViewport:
-  virtual void Init(const gfx::Rect& bounds) override {
-    platform_window_.reset(new ui::WinWindow(this, bounds));
-  }
-
-  virtual void Show() override {
-    platform_window_->Show();
-  }
-
-  virtual void Hide() override {
-    platform_window_->Hide();
-  }
-
-  virtual void Close() override {
-    platform_window_->Close();
-  }
-
-  virtual gfx::Size GetSize() override {
-    return platform_window_->GetBounds().size();
-  }
-
-  virtual void SetBounds(const gfx::Rect& bounds) override {
-    platform_window_->SetBounds(bounds);
-  }
-
-  virtual void SetCapture() override {
-    platform_window_->SetCapture();
-  }
-
-  virtual void ReleaseCapture() override {
-    platform_window_->ReleaseCapture();
-  }
-
-  // ui::PlatformWindowDelegate:
-  virtual void OnBoundsChanged(const gfx::Rect& new_bounds) override {
-    mojo::ViewportMetricsPtr metrics = mojo::ViewportMetrics::New();
-    metrics->size = new_bounds.size();
-    delegate_->OnMetricsChanged(metrics.Pass());
-  }
-
-  virtual void OnDamageRect(const gfx::Rect& damaged_region) override {
-  }
-
-  virtual void DispatchEvent(ui::Event* event) override {
-    delegate_->OnEvent(event);
-  }
-
-  virtual void OnCloseRequest() override {
-    platform_window_->Close();
-  }
-
-  virtual void OnClosed() override {
-    delegate_->OnDestroyed();
-  }
-
-  virtual void OnWindowStateChanged(ui::PlatformWindowState state) override {
-  }
-
-  virtual void OnLostCapture() override {
-  }
-
-  virtual void OnAcceleratedWidgetAvailable(
-      gfx::AcceleratedWidget widget) override {
-    delegate_->OnAcceleratedWidgetAvailable(widget);
-  }
-
-  virtual void OnActivationChanged(bool active) override {}
-
-  scoped_ptr<ui::PlatformWindow> platform_window_;
-  Delegate* delegate_;
-
-  DISALLOW_COPY_AND_ASSIGN(PlatformViewportWin);
-};
-
-// static
-scoped_ptr<PlatformViewport> PlatformViewport::Create(Delegate* delegate) {
-  return scoped_ptr<PlatformViewport>(new PlatformViewportWin(delegate)).Pass();
-}
-
-}  // namespace mojo

services/surfaces/surfaces_impl.h
@@ -25,8 +25,6 @@ class ApplicationManager;
 
 namespace surfaces {
 
-class SurfaceNativeViewportClient;
-
 class SurfacesImpl : public mojo::Surface,
                      public mojo::ViewportParameterListener,
                      public cc::SurfaceFactoryClient,

commit e10b476b5a4a5b16a153716ee930357b5f8c471b

Set initial size in sample_app
https://github.com/domokit/mojo/commit/91b812b1af824c8faba2f75d88d31a1532164728
broke sample_app since it threw away the initial viewport size and the
native viewport service's metrics changed callback no longer fires unless
the size changes. This uses the initial size as well.

R=sky@chromium.org

Review URL: https://codereview.chromium.org/885953002

examples/sample_app/sample_app.cc
@@ -82,6 +82,7 @@ class SampleApp : public mojo::ApplicationDelegate,
         native_viewport_id, metrics->size.Clone(), GetProxy(&command_buffer),
         listener.Pass());
     gles2_client_.reset(new GLES2ClientImpl(command_buffer.Pass()));
+    gles2_client_->SetSize(*metrics->size);
     viewport_->RequestMetrics(
         base::Bind(&SampleApp::OnMetricsChanged, base::Unretained(this)));
   }