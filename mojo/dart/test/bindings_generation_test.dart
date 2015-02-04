3bb38c190ea0dc341fd84645c2dc635cbba1f72e
Zachary Anderson <zra@google.com>
2015-02-04 12:40:08 -0800

    Dart: Fixes bug in array encoding.
    Also adds tests to bindings_generation_test.dart.
    
    BUG=
    R=jimbe@chromium.org
    
    Review URL: https://codereview.chromium.org/902573003

--- mojo/dart/test/bindings_generation_test.dart
+++ mojo/dart/test/bindings_generation_test.dart
@@ -10,7 +10,8 @@ import 'dart:typed_data';
 
 import 'package:mojo/dart/testing/expect.dart';
 import 'package:mojo/public/interfaces/bindings/tests/sample_interfaces.mojom.dart' as sample;
-
+import 'package:mojo/public/interfaces/bindings/tests/test_structs.mojom.dart' as structs;
+import 'package:mojo/public/interfaces/bindings/tests/rect.mojom.dart' as rect;
 
 class ProviderImpl extends sample.Provider {
   ProviderImpl(core.MojoMessagePipeEndpoint endpoint) : super(endpoint) {
@@ -37,7 +38,7 @@ void providerIsolate(core.MojoMessagePipeEndpoint endpoint) {
 }
 
 
-Future<bool> test() {
+Future<bool> testCallResponse() {
   var pipe = new core.MojoMessagePipe();
   var client = new sample.ProviderProxy(pipe.endpoints[0]);
   var c = new Completer();
@@ -57,7 +58,7 @@ Future<bool> test() {
 }
 
 
-Future testAwait() async {
+Future testAwaitCallResponse() async {
   var pipe = new core.MojoMessagePipe();
   var client = new sample.ProviderProxy(pipe.endpoints[0]);
   var isolate = await Isolate.spawn(providerIsolate, pipe.endpoints[1]);
@@ -73,7 +74,52 @@ Future testAwait() async {
 }
 
 
+bindings.ServiceMessage messageOfStruct(bindings.Struct s) =>
+  s.serializeWithHeader(new bindings.MessageHeader(0));
+
+
+testSerializeNamedRegion() {
+  var r = new rect.Rect()
+          ..x = 1
+          ..y = 2
+          ..width = 3
+          ..height = 4;
+  var namedRegion = new structs.NamedRegion()
+                    ..name = "name"
+                    ..rects = [r];
+  var message = messageOfStruct(namedRegion);
+  var namedRegion2 = structs.NamedRegion.deserialize(message.payload);
+  Expect.equals(namedRegion.name, namedRegion2.name);
+}
+
+
+testSerializeArrayValueTypes() {
+  var arrayValues = new structs.ArrayValueTypes()
+                    ..f0 = [0, 1, -1, 0x7f, -0x10]
+                    ..f1 = [0, 1, -1, 0x7fff, -0x1000]
+                    ..f2 = [0, 1, -1, 0x7fffffff, -0x10000000]
+                    ..f3 = [0, 1, -1, 0x7fffffffffffffff, -0x1000000000000000]
+                    ..f4 = [0.0, 1.0, -1.0, 4.0e9, -4.0e9]
+                    ..f5 = [0.0, 1.0, -1.0, 4.0e9, -4.0e9];
+  var message = messageOfStruct(arrayValues);
+  var arrayValues2 = structs.ArrayValueTypes.deserialize(message.payload);
+  Expect.listEquals(arrayValues.f0, arrayValues2.f0);
+  Expect.listEquals(arrayValues.f1, arrayValues2.f1);
+  Expect.listEquals(arrayValues.f2, arrayValues2.f2);
+  Expect.listEquals(arrayValues.f3, arrayValues2.f3);
+  Expect.listEquals(arrayValues.f4, arrayValues2.f4);
+  Expect.listEquals(arrayValues.f5, arrayValues2.f5);
+}
+
+
+testSerializeStructs() {
+  testSerializeNamedRegion();
+  testSerializeArrayValueTypes();
+}
+
+
 main() async {
-  await test();
-  await testAwait();
+  testSerializeStructs();
+  await testCallResponse();
+  await testAwaitCallResponse();
 }
