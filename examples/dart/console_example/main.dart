59036e5744e34628da5a89ad946c6e8fdf9f227d
Elliot Glaysher <erg@chromium.org>
2015-02-05 12:34:49 -0800

    Create a super simple console service.
    BUG=none
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/900083002

---
+++ examples/dart/console_example/main.dart
@@ -0,0 +1,33 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:mojo_application';
+import 'dart:mojo_bindings';
+import 'dart:mojo_core';
+
+import 'package:mojo/services/console/public/interfaces/console.mojom.dart';
+
+class ConsoleApplication extends Application {
+  ConsoleProxy _proxy;
+
+  ConsoleApplication.fromHandle(MojoHandle handle) : super.fromHandle(handle);
+
+  void initialize(List<String> args) {
+    _proxy = new ConsoleProxy.unbound();
+    connectToService("mojo:console", _proxy);
+
+    _proxy.readLine().then((result) {
+        _proxy.printLines([result.line]).then((result) {
+            _proxy.close();
+            close();
+          });
+      });
+  }
+}
+
+main(List args) {
+  var appHandle = new MojoHandle(args[0]);
+  var consoleApplication = new ConsoleApplication.fromHandle(appHandle);
+  consoleApplication.listen();
+}
---
+++ services/console/console.cc
@@ -0,0 +1,82 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+#include <string>
+
+#include "base/macros.h"
+#include "mojo/application/application_runner_chromium.h"
+#include "mojo/public/c/system/main.h"
+#include "mojo/public/cpp/application/application_connection.h"
+#include "mojo/public/cpp/application/application_delegate.h"
+#include "mojo/public/cpp/application/interface_factory.h"
+#include "mojo/public/cpp/bindings/interface_ptr.h"
+#include "mojo/services/console/public/interfaces/console.mojom.h"
+
+namespace mojo {
+
+class ConsoleImpl : public InterfaceImpl<Console> {
+ public:
+  explicit ConsoleImpl(const std::string& app_name) : app_name_(app_name) {}
+  ~ConsoleImpl() override {}
+
+  void ReadLine(const Callback<void(bool, String)>& callback) override {
+    std::cout << "[" << app_name_ << "]> ";
+    std::cout.flush();
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    // This will send an empty string on eof.
+    String return_value = String::From(line);
+    bool is_good = std::cin.good();
+    if (!is_good)
+      return_value = String::From("");
+
+    callback.Run(is_good, return_value);
+  }
+
+  void PrintLines(Array<mojo::String> data,
+                  const Callback<void(bool)>& callback) override {
+    for (size_t i = 0; i < data.size(); ++i)
+      std::cout << "[" << app_name_ << "] " << data[i].get() << std::endl;
+
+    callback.Run(true);
+  }
+
+ private:
+  const std::string app_name_;
+
+  DISALLOW_COPY_AND_ASSIGN(ConsoleImpl);
+};
+
+class ConsoleDelegate : public ApplicationDelegate,
+                        public InterfaceFactory<Console> {
+ public:
+  ConsoleDelegate() {}
+  ~ConsoleDelegate() override {}
+
+  // ApplicationDelegate implementation.
+  bool ConfigureIncomingConnection(ApplicationConnection* connection) override {
+    connection->AddService(this);
+    return true;
+  }
+
+  // InterfaceFactory<Console> implementation.
+  void Create(ApplicationConnection* connection,
+              InterfaceRequest<Console> request) override {
+    BindToRequest(new ConsoleImpl(connection->GetRemoteApplicationURL()),
+                  &request);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ConsoleDelegate);
+};
+
+}  // namespace mojo
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  mojo::ApplicationRunnerChromium runner(new mojo::ConsoleDelegate);
+  return runner.Run(shell_handle);
+}
