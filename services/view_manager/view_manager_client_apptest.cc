52a1b64b7f4f18f92017f8823a56c338d829671a
Alhaad Gokhale <alhaad@google.com>
2015-02-11 16:56:31 -0800

    Revert "Remove header mojo/public/interfaces/application/service_provider.mojom.h ."
    This reverts commit aef99534f6b1ce09e4d86c00f10f22b9578d7e03.
    
    TBR=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/922493002

--- services/view_manager/view_manager_client_apptest.cc
+++ services/view_manager/view_manager_client_apptest.cc
@@ -12,6 +12,7 @@
 #include "mojo/public/cpp/application/application_impl.h"
 #include "mojo/public/cpp/application/application_test_base.h"
 #include "mojo/public/cpp/application/service_provider_impl.h"
+#include "mojo/public/interfaces/application/service_provider.mojom.h"
 #include "mojo/services/geometry/public/cpp/geometry_util.h"
 #include "mojo/services/view_manager/public/cpp/lib/view_manager_client_impl.h"
 #include "mojo/services/view_manager/public/cpp/view_manager_client_factory.h"
