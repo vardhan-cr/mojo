80dd5075d247656a4a1c7860019a4454feba3042
Benjamin Lerman <qsr@chromium.org>
2015-03-18 13:51:24 +0100

    Move --nacl as gn phase only parameter.
    R=qsr@chromium.org
    
    Review URL: https://codereview.chromium.org/1011703002

--- mojo/tools/mojob.py
+++ mojo/tools/mojob.py
@@ -50,7 +50,8 @@ def _args_to_config(args):
       additional_args['use_goma'] = False
       additional_args['goma_dir'] = None
 
-  additional_args['use_nacl'] = args.nacl
+  if 'nacl' in args:
+    additional_args['use_nacl'] = args.nacl
 
   if 'dry_run' in args:
     additional_args['dry_run'] = args.dry_run
@@ -198,9 +199,6 @@ def main():
                              help='CPU architecture to build for.',
                              choices=['x64', 'x86', 'arm'])
 
-  parent_parser.add_argument('--nacl', help='Add in NaCl', default=False,
-                             action='store_true')
-
   subparsers = parser.add_subparsers()
 
   sync_parser = subparsers.add_parser('sync', parents=[parent_parser],
@@ -212,6 +210,9 @@ def main():
   gn_parser.set_defaults(func=gn)
   gn_parser.add_argument('--args', help='Specify extra args',
                          default=None, dest='gn_args')
+  gn_parser.add_argument('--nacl', help='Add in NaCl', default=False,
+                         action='store_true')
+
   clang_group = gn_parser.add_mutually_exclusive_group()
   clang_group.add_argument('--clang', help='Use Clang (default)', default=None,
                            action='store_true')
