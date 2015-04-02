f2a8c265b149b490c955177dfc11be77923453ca
Viet-Trung Luu <viettrungluu@chromium.org>
2015-04-02 13:54:32 -0700

    Fix < comparison against MOJO_RESULT_OK.
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1055873003

--- shell/native_application_support.cc
+++ shell/native_application_support.cc
@@ -114,9 +114,8 @@ bool RunNativeApplication(base::NativeLibrary app_library,
   // |MojoMain()| takes ownership of the service handle.
   MojoHandle handle = application_request.PassMessagePipe().release().value();
   MojoResult result = main_function(handle);
-  if (result < MOJO_RESULT_OK) {
-    LOG(ERROR) << "MojoMain returned error (result: " << result << ")";
-  }
+  LOG_IF(ERROR, result != MOJO_RESULT_OK)
+      << "MojoMain returned error (result: " << result << ")";
   return true;
 }
 
