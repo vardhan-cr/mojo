d1301ee848cc631bf5f34f868151c457655b14e1
Sergey Rogulenko <rogulenko@google.com>
2015-04-06 14:41:36 -0700

    go: set GOPATH when running the go tool
    If you have GOROOT set in your OS and Mojo uses
    a different go tool to build the code (like when you
    target android) the go tool shows an error.
    This patch fixes it.
    
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1061943002

--- mojo/go/go.py
+++ mojo/go/go.py
@@ -6,7 +6,7 @@
 """
 This script invokes the go build tool.
 Must be called as follows:
-python go.py <go-binary> <build directory> <output file> <src directory>
+python go.py <go-cmd> <build directory> <output file> <src directory>
 <CGO_CFLAGS> <CGO_LDFLAGS> <go-binary options>
 eg.
 python go.py /usr/lib/google-golang/bin/go out/build out/a.out .. "-I."
@@ -20,7 +20,7 @@ import sys
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('go_binary')
+  parser.add_argument('go_cmd')
   parser.add_argument('build_directory')
   parser.add_argument('output_file')
   parser.add_argument('src_root')
@@ -29,7 +29,7 @@ def main():
   parser.add_argument('cgo_ldflags')
   parser.add_argument('go_option', nargs='*')
   args = parser.parse_args()
-  go_binary = args.go_binary
+  go_cmd = args.go_cmd
   build_dir = args.build_directory
   out_file = os.path.abspath(args.output_file)
   # The src directory specified is relative. We need this as an absolute path.
@@ -48,9 +48,13 @@ def main():
   old_directory = os.getcwd()
   os.chdir(build_dir)
   os.environ["GOPATH"] = go_path
+  # Remove extra cmds if go_cmd is something like GOOS=android .../go
+  go_binary = go_cmd.split(" ")[-1]
+  bin_dir = os.path.dirname(os.path.abspath(go_binary))
+  os.environ["GOROOT"] = os.path.dirname(bin_dir)
   os.environ["CGO_CFLAGS"] = args.cgo_cflags
   os.environ["CGO_LDFLAGS"] = args.cgo_ldflags
-  os.system("%s %s" % (go_binary, " ".join(go_options)))
+  os.system("%s %s" % (go_cmd, " ".join(go_options)))
   out_files = [ f for f in os.listdir(".") if os.path.isfile(f)]
   if (len(out_files) > 0):
     shutil.move(out_files[0], out_file)
