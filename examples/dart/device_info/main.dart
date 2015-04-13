54afab19758f0a494998fceb3c0792b1152d4401
Alhaad Gokhale <alhaad@chromium.org>
2015-04-13 13:43:47 -0700

    Create a Mojo service to get device information at runtime.
    The DeviceInfo interface is implemented by two Mojo applications, one for Linux and another for Android. Both of them have the same output name and can be accessed irrespective of the device used. Also, added an example use.
    
    R=qsr@chromium.org, tonyg@chromium.org, hansmuller@chromium.org, jimbe@chromium.org
    
    Review URL: https://codereview.chromium.org/1073423002

---
+++ examples/dart/device_info/main.dart
@@ -0,0 +1,28 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:mojo/public/dart/application.dart';
+import 'package:mojo/public/dart/bindings.dart';
+import 'package:mojo/public/dart/core.dart';
+import 'package:mojo/services/device_info/public/interfaces/device_info.mojom.dart';
+
+class DeviceInfoApp extends Application {
+  final DeviceInfoProxy _deviceInfo = new DeviceInfoProxy.unbound();
+
+  DeviceInfoApp.fromHandle(MojoHandle handle) : super.fromHandle(handle);
+
+  Future initialize(List<String> args, String url) async {
+    connectToService("mojo:device_info", _deviceInfo);
+    print(await _deviceInfo.ptr.getDeviceType());
+    _deviceInfo.close();
+    close();
+  }
+}
+
+main(List args) {
+  MojoHandle appHandle = new MojoHandle(args[0]);
+  new DeviceInfoApp.fromHandle(appHandle);
+}
---
+++ services/device_info/device_info.cc
@@ -0,0 +1,56 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstdlib>
+
+#include "mojo/common/weak_binding_set.h"
+#include "mojo/public/c/system/main.h"
+#include "mojo/public/cpp/application/application_connection.h"
+#include "mojo/public/cpp/application/application_delegate.h"
+#include "mojo/public/cpp/application/application_runner.h"
+#include "mojo/public/cpp/application/interface_factory.h"
+#include "mojo/services/device_info/public/interfaces/device_info.mojom.h"
+
+namespace mojo {
+namespace services {
+namespace device_info {
+
+// This is a native Mojo application which implements |DeviceInfo| interface for
+// Linux.
+class DeviceInfo : public mojo::ApplicationDelegate,
+                   public mojo::DeviceInfo,
+                   public mojo::InterfaceFactory<mojo::DeviceInfo> {
+ public:
+  // We look for the 'DISPLAY' environment variable. If present, then we assume
+  // it to be a desktop, else we assume it to be a commandline
+  void GetDeviceType(const GetDeviceTypeCallback& callback) override {
+    callback.Run(getenv("DISPLAY") ? DEVICE_TYPE_DESKTOP
+                                   : DEVICE_TYPE_HEADLESS);
+  }
+
+  // |ApplicationDelegate| override.
+  bool ConfigureIncomingConnection(
+      mojo::ApplicationConnection* connection) override {
+    connection->AddService<mojo::DeviceInfo>(this);
+    return true;
+  }
+
+  // |InterfaceFactory<DeviceInfo>| implementation.
+  void Create(mojo::ApplicationConnection* connection,
+              mojo::InterfaceRequest<mojo::DeviceInfo> request) override {
+    binding_.AddBinding(this, request.Pass());
+  }
+
+ private:
+  mojo::WeakBindingSet<mojo::DeviceInfo> binding_;
+};
+
+}  // namespace device_info
+}  // namespace services
+}  // namespace mojo
+
+MojoResult MojoMain(MojoHandle application_request) {
+  mojo::ApplicationRunner runner(new mojo::services::device_info::DeviceInfo());
+  return runner.Run(application_request);
+}
