ea126fd4aaca5aa2bbdf799365558fe5e6df4909
Benjamin Lerman <qsr@chromium.org>
2015-05-20 11:09:59 +0200

    Revert multiple commits:
    Revert "Add master/slave versions of mojo::test::ScopedIPCSupport."
    This reverts commit f83fdf5b37804ed34a4dd2c13af8d953c73e06a4.
    
    Revert "Add an |AddSlaveAndBootstrap()| to MasterConnectionManager."
    This reverts commit a978a4ae86b7880d5d225c2bc37bbda284af806a.
    
    Revert "Update embedder API to allow master/slave processes to bootstrap and connect."
    This reverts commit dc5f26f3decbe75bf7290cad8aac3bc548972163.
    
    Reason for revert:
    dc5f26f3decbe75bf7290cad8aac3bc548972163. is breaking the unit tests on
    android. The 2 other commits are build over it.
    
    TBR=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/1150553002

--- mojo/edk/test/scoped_ipc_support.cc
+++ mojo/edk/test/scoped_ipc_support.cc
@@ -10,22 +10,17 @@
 namespace mojo {
 namespace test {
 
-namespace internal {
-
-ScopedIPCSupportBase::ScopedIPCSupportBase(
-    embedder::ProcessType process_type,
-    embedder::ProcessDelegate* process_delegate,
-    scoped_refptr<base::TaskRunner> io_thread_task_runner,
-    embedder::ScopedPlatformHandle platform_handle)
-    : io_thread_task_runner_(io_thread_task_runner.Pass()),
+ScopedIPCSupport::ScopedIPCSupport(
+    scoped_refptr<base::TaskRunner> io_thread_task_runner)
+    : io_thread_task_runner_(io_thread_task_runner),
       event_(true, false) {  // Manual reset.
   // Note: Run delegate methods on the I/O thread.
-  embedder::InitIPCSupport(process_type, io_thread_task_runner_,
-                           process_delegate, io_thread_task_runner_,
-                           platform_handle.Pass());
+  embedder::InitIPCSupport(embedder::ProcessType::NONE, io_thread_task_runner,
+                           this, io_thread_task_runner,
+                           embedder::ScopedPlatformHandle());
 }
 
-ScopedIPCSupportBase::~ScopedIPCSupportBase() {
+ScopedIPCSupport::~ScopedIPCSupport() {
   if (base::MessageLoop::current() &&
       base::MessageLoop::current()->task_runner() == io_thread_task_runner_) {
     embedder::ShutdownIPCSupportOnIOThread();
@@ -35,87 +30,8 @@ ScopedIPCSupportBase::~ScopedIPCSupportBase() {
   }
 }
 
-void ScopedIPCSupportBase::OnShutdownCompleteImpl() {
-  event_.Signal();
-}
-
-}  // namespace internal
-
-ScopedIPCSupport::ScopedIPCSupport(
-    scoped_refptr<base::TaskRunner> io_thread_task_runner)
-    : ScopedIPCSupportBase(embedder::ProcessType::NONE,
-                           this,
-                           io_thread_task_runner.Pass(),
-                           embedder::ScopedPlatformHandle()) {
-}
-
-ScopedIPCSupport::~ScopedIPCSupport() {
-}
-
 void ScopedIPCSupport::OnShutdownComplete() {
-  OnShutdownCompleteImpl();
-}
-
-ScopedMasterIPCSupport::ScopedMasterIPCSupport(
-    scoped_refptr<base::TaskRunner> io_thread_task_runner)
-    : ScopedIPCSupportBase(embedder::ProcessType::MASTER,
-                           this,
-                           io_thread_task_runner.Pass(),
-                           embedder::ScopedPlatformHandle()) {
-}
-
-ScopedMasterIPCSupport::ScopedMasterIPCSupport(
-    scoped_refptr<base::TaskRunner> io_thread_task_runner,
-    base::Callback<void(embedder::SlaveInfo slave_info)> on_slave_disconnect)
-    : ScopedIPCSupportBase(embedder::ProcessType::MASTER,
-                           this,
-                           io_thread_task_runner.Pass(),
-                           embedder::ScopedPlatformHandle()),
-      on_slave_disconnect_(on_slave_disconnect) {
-}
-
-ScopedMasterIPCSupport::~ScopedMasterIPCSupport() {
-}
-
-void ScopedMasterIPCSupport::OnShutdownComplete() {
-  OnShutdownCompleteImpl();
-}
-
-void ScopedMasterIPCSupport::OnSlaveDisconnect(embedder::SlaveInfo slave_info) {
-  if (!on_slave_disconnect_.is_null())
-    on_slave_disconnect_.Run(slave_info);
-}
-
-ScopedSlaveIPCSupport::ScopedSlaveIPCSupport(
-    scoped_refptr<base::TaskRunner> io_thread_task_runner,
-    embedder::ScopedPlatformHandle platform_handle)
-    : ScopedIPCSupportBase(embedder::ProcessType::SLAVE,
-                           this,
-                           io_thread_task_runner.Pass(),
-                           platform_handle.Pass()) {
-}
-
-ScopedSlaveIPCSupport::ScopedSlaveIPCSupport(
-    scoped_refptr<base::TaskRunner> io_thread_task_runner,
-    embedder::ScopedPlatformHandle platform_handle,
-    base::Closure on_master_disconnect)
-    : ScopedIPCSupportBase(embedder::ProcessType::SLAVE,
-                           this,
-                           io_thread_task_runner.Pass(),
-                           platform_handle.Pass()),
-      on_master_disconnect_(on_master_disconnect) {
-}
-
-ScopedSlaveIPCSupport::~ScopedSlaveIPCSupport() {
-}
-
-void ScopedSlaveIPCSupport::OnShutdownComplete() {
-  OnShutdownCompleteImpl();
-}
-
-void ScopedSlaveIPCSupport::OnMasterDisconnect() {
-  if (!on_master_disconnect_.is_null())
-    on_master_disconnect_.Run();
+  event_.Signal();
 }
 
 }  // namespace test
