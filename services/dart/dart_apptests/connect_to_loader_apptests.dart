e6762a8a506aa16f5f24216c04bb9406a2f800b3
Benjamin Lerman <qsr@chromium.org>
2015-06-04 11:56:15 +0200

    Testing than dart can connect to application handled by a loader.
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org/1148663004

---
+++ services/dart/dart_apptests/connect_to_loader_apptests.dart
@@ -0,0 +1,26 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library connectToLoader_apptests;
+
+import 'dart:async';
+
+import 'package:apptest/apptest.dart';
+import 'package:mojom/mojo/url_response_disk_cache.mojom.dart';
+import 'package:mojom/mojo/url_response.mojom.dart';
+import 'package:mojo/application.dart';
+import 'package:mojo/bindings.dart';
+import 'package:mojo/core.dart';
+
+connectToLoaderApptests(Application application, String url) {
+  test('Connection', () async {
+    var diskCacheProxy = new UrlResponseDiskCacheProxy.unbound();
+    application.connectToService("mojo:url_response_disk_cache",
+        diskCacheProxy);
+    var response = new UrlResponse();
+    response.url = 'http://www.example.com';
+    await diskCacheProxy.ptr.getFile(response);
+    await diskCacheProxy.close();
+  });
+}
--- services/dart/dart_apptests/main.dart
+++ services/dart/dart_apptests/main.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'package:apptest/apptest.dart';
 import 'package:mojom/dart/test/echo_service.mojom.dart';
 
+import 'connect_to_loader_apptests.dart' as connect_to_loader_apptests;
 import 'echo_apptests.dart' as echo;
 import 'pingpong_apptests.dart' as pingpong;
 import 'io_http_apptests.dart' as io_http;
@@ -14,6 +15,7 @@ import 'versioning_apptests.dart' as versioning;
 
 Future<int> main(List args) async {
   final tests = [
+    connect_to_loader_apptests.connectToLoaderApptests,
     echo.echoApptests,
     io_internet_address.tests,
     io_http.tests,
