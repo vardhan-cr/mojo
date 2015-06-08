df0bfacad3ddf3a5b58803b2d978cb9f51499a91
Alexandre Zani <azani@chromium.org>
2015-06-08 14:02:18 -0700

    Unions in mojom in dart.
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org/1153543013

--- mojo/dart/test/bindings_generation_test.dart
+++ mojo/dart/test/bindings_generation_test.dart
@@ -11,6 +11,7 @@ import 'package:mojo/bindings.dart' as bindings;
 import 'package:mojo/core.dart' as core;
 import 'package:mojom/sample/sample_interfaces.mojom.dart' as sample;
 import 'package:mojom/mojo/test/test_structs.mojom.dart' as structs;
+import 'package:mojom/mojo/test/test_unions.mojom.dart' as unions;
 import 'package:mojom/mojo/test/rect.mojom.dart' as rect;
 
 class ProviderImpl implements sample.Provider {
@@ -111,6 +112,114 @@ testSerializeStructs() {
   testSerializeArrayValueTypes();
 }
 
+testSerializePodUnions() {
+  var s = new unions.WrapperStruct()
+    ..podUnion = new unions.PodUnionWrapper();
+  s.podUnion.fFUint32 = 32;
+
+  Expect.equals(unions.PodUnionTag.tagFUint32, s.podUnion.tag);
+  Expect.equals(32, s.podUnion.fFUint32);
+
+  var message = messageOfStruct(s);
+  var s2 = unions.WrapperStruct.deserialize(message.payload);
+
+  Expect.equals(s.podUnion.fFUint32, s2.podUnion.fFUint32);
+}
+
+testSerializeStructInUnion() {
+  var s = new unions.WrapperStruct()
+    ..objectUnion = new unions.ObjectUnionWrapper();
+  s.objectUnion.fFDummy = new unions.DummyStruct()
+    ..fInt8 = 8;
+
+  var message = messageOfStruct(s);
+  var s2 = unions.WrapperStruct.deserialize(message.payload);
+
+  Expect.equals(s.objectUnion.fFDummy.fInt8, s2.objectUnion.fFDummy.fInt8);
+}
+
+testSerializeArrayInUnion() {
+  var s = new unions.WrapperStruct()
+    ..objectUnion = new unions.ObjectUnionWrapper();
+  s.objectUnion.fFArrayInt8 = [1, 2, 3];
+
+  var message = messageOfStruct(s);
+  var s2 = unions.WrapperStruct.deserialize(message.payload);
+
+  Expect.listEquals(s.objectUnion.fFArrayInt8, s2.objectUnion.fFArrayInt8);
+}
+
+testSerializeMapInUnion() {
+  var s = new unions.WrapperStruct()
+    ..objectUnion = new unions.ObjectUnionWrapper();
+  s.objectUnion.fFMapInt8 = {
+    "one": 1,
+    "two": 2,
+  };
+
+  var message = messageOfStruct(s);
+  var s2 = unions.WrapperStruct.deserialize(message.payload);
+
+  Expect.equals(1, s.objectUnion.fFMapInt8["one"]);
+  Expect.equals(2, s.objectUnion.fFMapInt8["two"]);
+}
+
+testSerializeUnionInArray() {
+  var s = new unions.SmallStruct()
+    ..podUnionArray = [
+      new unions.PodUnionWrapper()
+        ..fFUint16 = 16,
+      new unions.PodUnionWrapper()
+        ..fFUint32 = 32,
+    ];
+
+  var message = messageOfStruct(s);
+
+  var s2 = unions.SmallStruct.deserialize(message.payload);
+
+  Expect.equals(16, s2.podUnionArray[0].fFUint16);
+  Expect.equals(32, s2.podUnionArray[1].fFUint32);
+}
+
+testSerializeUnionInMap() {
+  var s = new unions.SmallStruct()
+    ..podUnionMap = {
+      'one': new unions.PodUnionWrapper()
+        ..fFUint16 = 16,
+      'two': new unions.PodUnionWrapper()
+        ..fFUint32 = 32,
+    };
+
+  var message = messageOfStruct(s);
+
+  var s2 = unions.SmallStruct.deserialize(message.payload);
+
+  Expect.equals(16, s2.podUnionMap['one'].fFUint16);
+  Expect.equals(32, s2.podUnionMap['two'].fFUint32);
+}
+
+testSerializeUnionInUnion() {
+  var s = new unions.WrapperStruct()
+    ..objectUnion = new unions.ObjectUnionWrapper();
+    s.objectUnion.fFPodUnion = new unions.PodUnionWrapper()
+        ..fFUint32 = 32;
+
+  var message = messageOfStruct(s);
+  var s2 = unions.WrapperStruct.deserialize(message.payload);
+
+  Expect.equals(32, s2.objectUnion.fFPodUnion.fFUint32);
+}
+
+testSerializeUnions() {
+  testSerializePodUnions();
+  testSerializeStructInUnion();
+  testSerializeArrayInUnion();
+  testSerializeMapInUnion();
+  testSerializeUnionInArray();
+  testSerializeUnionInMap();
+  testSerializeUnionInUnion();
+}
+
 void closingProviderIsolate(core.MojoMessagePipeEndpoint endpoint) {
   var provider = new ProviderImpl(endpoint);
   provider._stub.close();
@@ -129,6 +238,7 @@ Future<bool> runOnClosedTest() {
 
 main() async {
   testSerializeStructs();
+  testSerializeUnions();
   await testCallResponse();
   await testAwaitCallResponse();
   await runOnClosedTest();
