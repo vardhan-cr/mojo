vardhan-cr/mojo

commit 52ca1a43f7fca3f0a26b2e98afa9ec6ba6e5e0b2

Fix native_viewport_service to always create viewports on the current activity.
This allows several native viewport clients to be launched and have each
one draw on a separate surface, on different tasks. This is done by
storing the current activity context and making it globally available.

Note that apps embedded in a window manager still cannot be displayed on
separate Android tasks.

Tracking feature bug: #292

R=qsr@chromium.org

Review URL: https://codereview.chromium.org/1221253003.

shell/android/apk/src/org/chromium/mojo/shell/ShellMain.java
@@ -135,6 +135,7 @@ public class ShellMain {
             Log.e(TAG, "ShellMain initialization failed.", e);
             throw new RuntimeException(e);
         }
+        start();
     }
 
     /**

services/authentication/src/org/chromium/mojo/authentication/AuthenticationServiceImpl.java
@@ -5,7 +5,6 @@
 package org.chromium.mojo.authentication;
 
 import android.accounts.AccountManager;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Parcel;
@@ -79,14 +78,14 @@ public class AuthenticationServiceImpl
         }
     }
 
-    private final Activity mContext;
+    private final Context mContext;
     private final String mConsumerURL;
     private final IntentReceiverManager mIntentReceiverManager;
     private Db mDb = null;
     private File mDbFile = null;
 
     public AuthenticationServiceImpl(Context context, Core core, String consumerURL, Shell shell) {
-        mContext = (Activity) context;
+        mContext = context;
         mConsumerURL = consumerURL;
         mIntentReceiverManager = ShellHelper.connectToService(
                 core, shell, "mojo:intent_receiver", IntentReceiverManager.MANAGER);

services/native_viewport/android/src/org/chromium/mojo/PlatformViewportAndroid.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
 import org.chromium.mojo.keyboard.KeyboardServiceImpl;
@@ -28,8 +29,8 @@ public class PlatformViewportAndroid extends SurfaceView {
     private final SurfaceHolder.Callback mSurfaceCallback;
 
     @CalledByNative
-    public static PlatformViewportAndroid createForActivity(
-            Activity activity, long nativeViewport) {
+    public static PlatformViewportAndroid create(long nativeViewport) {
+        Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
         PlatformViewportAndroid rv = new PlatformViewportAndroid(activity, nativeViewport);
         KeyboardServiceImpl.setActiveView(rv);
         activity.setContentView(rv);

services/native_viewport/platform_viewport_android.cc
@@ -149,9 +149,9 @@ bool PlatformViewportAndroid::TouchEvent(JNIEnv* env,
 void PlatformViewportAndroid::Init(const gfx::Rect& bounds) {
   JNIEnv* env = base::android::AttachCurrentThread();
   java_platform_viewport_android_ = JavaObjectWeakGlobalRef(
-      env, Java_PlatformViewportAndroid_createForActivity(
-               env, base::android::GetApplicationContext(),
-               reinterpret_cast<jlong>(this)).obj());
+      env,
+      Java_PlatformViewportAndroid_create(env, reinterpret_cast<jlong>(this))
+          .obj());
 }
 
 void PlatformViewportAndroid::Show() {

shell/android/apk/AndroidManifest.xml.jinja2
@@ -24,7 +24,8 @@
         <meta-data android:name="com.google.android.gms.version"
                    android:value="@integer/google_play_services_version" />
         <activity android:name="org.chromium.mojo.shell.MojoShellActivity"
-                  android:launchMode="singleTask"
+                  android:launchMode="standard"
+                  android:documentLaunchMode="intoExisting"
                   android:theme="@android:style/Theme.Holo.Light.NoActionBar"
                   android:configChanges="orientation|keyboardHidden|keyboard|screenSize"
                   android:windowSoftInputMode="adjustPan"

shell/android/apk/src/org/chromium/mojo/shell/MojoShellActivity.java
@@ -33,13 +33,8 @@ public class MojoShellActivity extends Activity {
         // has no obligation to kill the application process between destroying and restarting the
         // activity. If the application process is kept alive, initialization parameters sent with
         // the intent will be stale.
-        // TODO(qsr): We should be passing application context here as required by
-        // InitApplicationContext on the native side. Currently we can't, as PlatformViewportAndroid
-        // relies on this being the activity context.
-        ShellMain.ensureInitialized(this, getParametersFromIntent(getIntent()));
-
         // TODO(eseidel): ShellMain can fail, but we're ignoring the return.
-        ShellMain.start();
+        ShellMain.ensureInitialized(getApplicationContext(), getParametersFromIntent(getIntent()));
 
         onNewIntent(getIntent());
 

commit 421630295997da0d2d29d45029c715bacbd54414

Close all activities when the shell exits
This fixes a bug introduced by 52ca1a43f7fca3f0a26b2e98afa9ec6ba6e5e0b2,
where a Context obtained through Context.getApplicationContext() is cast
as an Activity in ShellMain.finishActivity().

R=qsr@chromium.org, qsr
BUG=

Review URL: https://codereview.chromium.org/1269063002 .

shell/android/apk/src/org/chromium/mojo/shell/ShellMain.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
 import org.chromium.mojo.bindings.InterfaceRequest;
@@ -19,6 +20,7 @@ import org.chromium.mojom.mojo.ServiceProvider;
 import org.chromium.mojom.mojo.Shell;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -176,8 +178,13 @@ public class ShellMain {
     }
 
     @CalledByNative
-    private static void finishActivity(Activity activity) {
-        activity.finish();
+    private static void finishActivities() {
+        for (WeakReference<Activity> activityRef : ApplicationStatus.getRunningActivities()) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+                activity.finish();
+            }
+        }
     }
 
     /**

shell/android/main.cc
@@ -83,9 +83,6 @@ struct InternalShellData {
   // Thread to run the shell on.
   scoped_ptr<base::DelegateSimpleThread> shell_thread;
 
-  // Main android app activity, to be used on the main thread.
-  base::android::ScopedJavaGlobalRef<jobject> main_activity;
-
   // Proxy to a Window Manager, initialized on the shell thread.
   mojo::WindowManagerPtr window_manager;
 
@@ -134,8 +131,7 @@ void ConfigureAndroidServices(Context* context) {
 void QuitShellThread() {
   g_internal_data.Get().shell_thread->Join();
   g_internal_data.Get().shell_thread.reset();
-  Java_ShellMain_finishActivity(base::android::AttachCurrentThread(),
-                                g_internal_data.Get().main_activity.obj());
+  Java_ShellMain_finishActivities(base::android::AttachCurrentThread());
   exit(0);
 }
 
@@ -206,13 +202,12 @@ void ConnectToApplicationImpl(
 
 static void Start(JNIEnv* env,
                   jclass clazz,
-                  jobject activity,
+                  jobject application_context,
                   jstring mojo_shell_child_path,
                   jobjectArray jparameters,
                   jstring j_local_apps_directory,
                   jstring j_tmp_dir,
                   jstring j_home_dir) {
-  g_internal_data.Get().main_activity.Reset(env, activity);
   // Initially, the shell runner is not ready.
   g_internal_data.Get().shell_runner_ready.reset(
       new base::WaitableEvent(true, false));
@@ -228,8 +223,9 @@ static void Start(JNIEnv* env,
       1);
   DCHECK_EQ(return_value, 0);
 
-  base::android::ScopedJavaLocalRef<jobject> scoped_activity(env, activity);
-  base::android::InitApplicationContext(env, scoped_activity);
+  base::android::ScopedJavaLocalRef<jobject> scoped_application_context(
+      env, application_context);
+  base::android::InitApplicationContext(env, scoped_application_context);
 
   std::vector<std::string> parameters;
   parameters.push_back("mojo_shell");