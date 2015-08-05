a40f567aefdf203767d953161450d08e0bb52995
Przemyslaw Pietrzkiewicz <ppi@chromium.org>
2015-08-05 16:00:23 +0200

    Add mojo shebangs for dart examples.
    An alternative would be to map dart_content_handler as default handler
    for application/dart mime type, but this would depend on server
    configuration returning the correct mime type.
    
    R=qsr@chromium.org
    
    Review URL: https://codereview.chromium.org/1277463002 .

--- examples/dart/camera_roll/camera_roll.dart
+++ examples/dart/camera_roll/camera_roll.dart
@@ -1,3 +1,4 @@
+#!mojo sky_viewer.mojo
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
