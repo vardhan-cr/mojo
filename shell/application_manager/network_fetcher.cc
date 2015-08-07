aadd27ade90fb1a8525e404bc4773e81aca1e51c
Benjamin Lerman <qsr@chromium.org>
2015-08-07 17:20:46 +0200

    Fix incorrect fetcher option.
    R=etiennej@chromium.org
    TBR=etiennej@chromium.org
    
    Review URL: https://codereview.chromium.org/1282443003 .

--- shell/application_manager/network_fetcher.cc
+++ shell/application_manager/network_fetcher.cc
@@ -149,7 +149,7 @@ void NetworkFetcher::StartNetworkRequest(RequestType request_type) {
   request->url = mojo::String::From(url_);
   request->auto_follow_redirects = false;
   if (disable_cache_)
-    request->cache_mode = mojo::URLRequest::CACHE_MODE_ONLY_FROM_CACHE;
+    request->cache_mode = mojo::URLRequest::CACHE_MODE_BYPASS_CACHE;
   auto header = mojo::HttpHeader::New();
   header->name = "X-Architecture";
   header->value = kArchitecture;
