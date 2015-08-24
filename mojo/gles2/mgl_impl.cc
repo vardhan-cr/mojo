aec09caa26b7b0e08d3372aef64beadb68f79dd2
James Robinson <jamesr@chromium.org>
2015-08-24 10:37:59 -0700

    Factor control thunks into common utility, implement MGLGetCurrentContext
    This factors the MGL/MojoGLES2 control thunks, like creating a context,
    into one gles2::ControlThunksImpl class that manages the TLS slot for
    the current context and type casts. This allows implementing the
    MGLGetCurrentContext entry point and more cleanly sharing the
    implementations of the redudant MGL/GLES2 entry points. In the future
    when the redundant entry points are removing having the control thunks
    be separate from gles2 thunks will still be useful as the gles3/3.1/etc
    thunks will still want to share these control entry points.
    
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/1285373005 .

--- mojo/gles2/mgl_impl.cc
+++ mojo/gles2/mgl_impl.cc
@@ -4,16 +4,11 @@
 
 // This file implements the MGL and MGL onscreen entry points exposed to the
 // Mojo application by the shell.
-//
-// TODO(jamesr): These entry points are implemented on top of the MojoGLES2
-// family, which for control functions is backwards.  The MojoGLES2 control
-// entry points should be implemented on top of these entry points while we
-// deprecate them.
 
+#include "mojo/gles2/control_thunks_impl.h"
 #include "mojo/public/c/gles2/gles2.h"
 #include "mojo/public/c/gpu/MGL/mgl.h"
 #include "mojo/public/c/gpu/MGL/mgl_onscreen.h"
-#include "mojo/public/cpp/system/message_pipe.h"
 
 extern "C" {
 
@@ -23,50 +18,29 @@ MGLContext MGLCreateContext(MGLOpenGLAPIVersion version,
                             MGLContextLostCallback lost_callback,
                             void* lost_callback_closure,
                             const struct MojoAsyncWaiter* async_waiter) {
-  // TODO(jamesr): Support ES 3.0 / 3.1 where possible.
-  if (version != MGL_API_VERSION_GLES2) {
-    MojoClose(command_buffer_handle);
-    return MGL_NO_CONTEXT;
-  }
-  // TODO(jamesr): Plumb through share groups.
-  if (share_group != MGL_NO_CONTEXT) {
-    MojoClose(command_buffer_handle);
-    return MGL_NO_CONTEXT;
-  }
-
-  return reinterpret_cast<MGLContext>(
-      MojoGLES2CreateContext(command_buffer_handle, lost_callback,
-                             lost_callback_closure, async_waiter));
+  return gles2::ControlThunksImpl::Get()->CreateContext(
+      version, command_buffer_handle, share_group, lost_callback,
+      lost_callback_closure, async_waiter);
 }
 
 void MGLDestroyContext(MGLContext context) {
-  MojoGLES2DestroyContext(reinterpret_cast<MojoGLES2Context>(context));
+  return gles2::ControlThunksImpl::Get()->DestroyContext(context);
 }
 
 void MGLMakeCurrent(MGLContext context) {
-  MojoGLES2MakeCurrent(reinterpret_cast<MojoGLES2Context>(context));
-  // TODO(jamesr): Plumb through loss information.
+  return gles2::ControlThunksImpl::Get()->MakeCurrent(context);
 }
 
 MGLContext MGLGetCurrentContext() {
-  // TODO(jamesr): Implement.
-  return MGL_NO_CONTEXT;
+  return gles2::ControlThunksImpl::Get()->GetCurrentContext();
 }
 
-// TODO(jamesr): Hack - this symbol is defined in //mojo/gles2/gles2_impl.cc.
-// What should really happen here is this should call into the data structure
-// underlying MGLContext, but that can't happen until control is inverted.
-void MojoGLES2glResizeCHROMIUM(uint32_t width,
-                               uint32_t height,
-                               float scale_factor);
-
 void MGLResizeSurface(uint32_t width, uint32_t height) {
-  // TODO(jamesr): Implement
-  MojoGLES2glResizeCHROMIUM(width, height, 1.f);
+  return gles2::ControlThunksImpl::Get()->ResizeSurface(width, height);
 }
 
 void MGLSwapBuffers() {
-  MojoGLES2SwapBuffers();
+  return gles2::ControlThunksImpl::Get()->SwapBuffers();
 }
 
 }  // extern "C"
