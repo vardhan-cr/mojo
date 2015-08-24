8920b6de2d0f86438768bd2eeb17bac18519a2d4
John McCutchan <johnmccutchan@google.com>
2015-08-24 14:29:07 -0700

    Fix mojo dart:io IPv6 address printing
    BUG=
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1309313002 .

--- services/dart/dart_apptests/lib/src/io_internet_address_apptests.dart
+++ services/dart/dart_apptests/lib/src/io_internet_address_apptests.dart
@@ -33,6 +33,13 @@ tests(Application application, String url) {
       expect(result.length, greaterThan(0));
       expect(result[0], equals(InternetAddress.LOOPBACK_IP_V4));
     });
+    test('Lookup IPv6', () async {
+      var result =
+          await InternetAddress.lookup('localhost',
+                                       type:InternetAddressType.IP_V6);
+      expect(result.length, greaterThan(0));
+      expect(result[0], equals(InternetAddress.LOOPBACK_IP_V6));
+    });
     test('Lookup ANY', () async {
       var result =
           await InternetAddress.lookup('localhost',
