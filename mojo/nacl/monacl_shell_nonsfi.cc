6ab4c30e79ebc23d8d3a8c96ad0f25aead00d7a4
Sean Klein <smklein@google.com>
2015-08-27 15:44:07 -0700

    Late responses to code review in https://codereview.chromium.org/1309733003/
    BUG=https://github.com/domokit/mojo/issues/396
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1310883009 .

--- mojo/nacl/monacl_shell_nonsfi.cc
+++ mojo/nacl/monacl_shell_nonsfi.cc
@@ -35,7 +35,7 @@ const struct nacl_irt_mojo kIrtMojo = {
   nullptr, // TODO(smklein): Add _MojoGetInitialHandle.
 };
 
-const struct nacl_irt_interface irt_interfaces[] = {
+const struct nacl_irt_interface kIrtInterfaces[] = {
   { NACL_IRT_MOJO_v0_1, &kIrtMojo, sizeof(kIrtMojo), nullptr }
 };
 
@@ -44,8 +44,8 @@ size_t mojo_irt_nonsfi_query(const char* interface_ident,
   size_t result = nacl_irt_query_list(interface_ident,
                                       table,
                                       tablesize,
-                                      irt_interfaces,
-                                      sizeof(irt_interfaces));
+                                      kIrtInterfaces,
+                                      sizeof(kIrtInterfaces));
   if (result != 0)
     return result;
   return nacl_irt_query_core(interface_ident, table, tablesize);
@@ -59,11 +59,11 @@ int main(int argc, char** argv, char** environ) {
     std::cerr << "Usage: " << argv[0] <<  " <executable> <args...>\n";
     return 1;
   }
-  const char *nexe_filename = argv[1];
+  const char* nexe_filename = argv[1];
   int fd = open(nexe_filename, O_RDONLY);
   if (fd < 0) {
     std::cerr << "Failed to open " << nexe_filename << ": " <<
-              strerror(errno) << std::endl;
+              strerror(errno) << "\n";
     return 1;
   }
   uintptr_t entry = NaClLoadElfFile(fd);
