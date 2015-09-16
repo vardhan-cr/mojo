d90eccafc7c17592dcae73f48f5002b2afa3db6c
Viet-Trung Luu <viettrungluu@chromium.org>
2015-09-16 16:11:23 -0700

    Use MOJO_MOVE_ONLY_TYPE instead of base's MOVE_ONLY_TYPE_WITH_MOVE_CONSTRUCTOR_FOR_CPP_03 in scoped_platform_handle.h.
    R=vardhan@google.com
    
    Review URL: https://codereview.chromium.org/1345383002 .

--- mojo/edk/embedder/scoped_platform_handle.h
+++ mojo/edk/embedder/scoped_platform_handle.h
@@ -5,10 +5,10 @@
 #ifndef MOJO_EDK_EMBEDDER_SCOPED_PLATFORM_HANDLE_H_
 #define MOJO_EDK_EMBEDDER_SCOPED_PLATFORM_HANDLE_H_
 
-#include "base/move.h"
 #include "mojo/edk/embedder/platform_handle.h"
 #include "mojo/edk/system/system_impl_export.h"
 #include "mojo/public/c/system/macros.h"
+#include "mojo/public/cpp/system/macros.h"
 
 namespace mojo {
 namespace embedder {
@@ -16,7 +16,7 @@ namespace embedder {
 // Scoper for |PlatformHandle|s, which are file descriptors on POSIX and
 // |HANDLE|s on Windows.
 class MOJO_SYSTEM_IMPL_EXPORT ScopedPlatformHandle {
-  MOVE_ONLY_TYPE_WITH_MOVE_CONSTRUCTOR_FOR_CPP_03(ScopedPlatformHandle)
+  MOJO_MOVE_ONLY_TYPE(ScopedPlatformHandle)
 
  public:
   ScopedPlatformHandle() {}
