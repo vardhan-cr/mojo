63b3571321c86d4a2425303a95fb66d70453b917
Vardhan Mudunuru <vardhan-cr@chromium.org>
2015-09-28 18:06:33 -0700

    public serialization API

---
+++ mojo/public/cpp/bindings/tests/serialization_api_unittest.cc
@@ -0,0 +1,12 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace mojo {
+namespace test {
+
+TEST(StructSerializationAPI, Basic) {
+}
+
+}  // namespace test
+}  // namespace mojo
