d081cba93ac2de792d0c27e2afe2820b138a1266
Viet-Trung Luu <viettrungluu@chromium.org>
2015-09-29 10:53:30 -0700

    Rename mojo_message_pipe_perftests -> mojo_system_perftests and run it on the bots.
    ("mojob.py perftest" will now run those perftests.)
    
    R=vardhan@google.com
    
    Review URL: https://codereview.chromium.org/1376893002 .

--- mojo/tools/get_test_list.py
+++ mojo/tools/get_test_list.py
@@ -187,7 +187,8 @@ def GetTestList(config, verbose_count=0):
   if target_os == Config.OS_LINUX and ShouldRunTest(Config.TEST_TYPE_PERF):
     perf_id = "linux_%s" % ("debug" if config.is_debug else "release")
     test_names = ["mojo_public_system_perftests",
-                  "mojo_public_bindings_perftests"]
+                  "mojo_public_bindings_perftests",
+                  "mojo_system_perftests"]
 
     for test_name in test_names:
       command = ["python",
